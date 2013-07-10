26201546a00c0e02ba0af2accda8d64b8fce1e98
Oleg Gaidarenko <arkel@yandex-team.ru>
2013-07-11 00:46:17 +0400

    Add skeleton for test suite

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,32 @@
 "use strict";
+
 module.exports = function( grunt ) {
+
     grunt.initConfig({
-        pkg: grunt.file.readJSON( "package.json" )
+        pkg: grunt.file.readJSON( "package.json" ),
+
+        watch: {
+            express: {
+                files:  [ "*.js" ],
+                tasks:  [ "express:dev" ],
+                options: {
+                    nospawn: true //Without this option specified express won't be reloaded
+                }
+            }
+        },
+
+        express: {
+            dev: {
+                options: {
+                    script: "server.js"
+                }
+            }
+        }
     });
+
+    grunt.loadNpmTasks( "grunt-contrib-watch" );
+    grunt.loadNpmTasks( "grunt-express-server" );
+
+    grunt.registerTask( "default", [ "express", "watch" ] );
+    grunt.registerTask( "server", "default" );
 };
