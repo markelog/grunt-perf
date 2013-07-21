687c9080fde655e9013c1ea7a8571a110c2999b3
Oleg Gaidarenko <arkel@yandex-team.ru>
2013-07-21 19:05:02 +0400

    Calculate real test statistic
    But only for one test at the moment

--- tasks/perf.js
+++ tasks/perf.js
@@ -17,36 +17,36 @@ module.exports = function( grunt ) {
         grunt.log.oklns( "Test started" );
     });
 
-    grunt.registerTask( "perf", function( name ) {
+    grunt.registerTask( "perf", function() {
         var first = true,
             done = this.async(),
 
             options = this.options(),
+            dist = app + "/" + options.dist,
 
-            perf = new Perf( options, start );
+            perf = new Perf( options );
 
+        phantomjs.on( "complete", function( data, event ) {
+            grunt.log.ok( "Test finished" );
 
-        function start() {
-            phantomjs.on( "complete", function( data, event ) {
-                grunt.log.ok( "Test finished" );
+            perf.put( data );
 
-                perf.put( data );
+            if ( first ) {
+                first = false;
 
-                if ( first ) {
-                    first = false;
+                perf.erenow(function() {
+                    this.checkout(function() {
+                        this.prepare( dist ).start();
+                    });
+                });
 
-                    perf.checkout( perf.start );
+            } else {
+                perf.finish();
+                done();
+            }
+        });
 
-
-                } else {
-                    grunt.file.delete( ".perf" );
-                    grunt.file.write( ".test", JSON.stringify( perf.benchmarks.replaceWith.results ) );
-                    done();
-                }
-            });
-
-            this.start();
-        }
+        perf.start();
     });
 
 
@@ -55,6 +55,9 @@ module.exports = function( grunt ) {
 
         this.benchmarks = benchmarks = {};
         this.options = options;
+        this.reference = grunt.option( "reference" ) || grunt.option( "hash" ) ||
+            grunt.option( "tag" ) || grunt.option( "branch" ) || "HEAD~1"
+
 
         grunt.file.expand( options.benchmarks ).forEach(function( name ) {
             var benchmark = benchmarks[ name.split( "/" )[ 1 ] ] = {
@@ -66,7 +69,7 @@ module.exports = function( grunt ) {
             });
         });
 
-        return this.prepare( fn );
+        return this.prepare();
     };
 
     Perf.formatNumber = function( number ) {
@@ -80,23 +83,40 @@ module.exports = function( grunt ) {
 
         listen: null,
 
-        spawn: function() {
-            phantomjs.spawn( "http://localhost:7000", {
-                options: {}
+        prepare: function( dist ) {
+            dist = dist || this.options.dist;
+
+            var suite,
+                d = tmp + "/dist.js",
+                self = this;
+
+            if ( grunt.file.exists( tmp ) ) {
+                grunt.file.copy( dist, d );
+
+                return this;
+            }
+
+            grunt.file.mkdir( app );
+
+            suite = grunt.file.read( __dirname + "/../suite/suite.html" );
+
+            suite = suite.replace( "{{js}}", this.benchmarks.replaceWith.js );
+            suite = suite.replace( "{{html}}", this.benchmarks.replaceWith.html );
+
+            grunt.file.copy( dist, d );
+
+            grunt.file.recurse( __dirname + "/../suite/", function( abspath, rootdir, subdir, filename ) {
+                grunt.file.copy( abspath, ".perf/" + filename );
             });
+
+            grunt.file.write( ".perf/index.html", suite );
 
             return this;
         },
 
-        prepare: function( fn ) {
-            var suite,
-                self = this;
-
-            if ( grunt.file.exists( tmp ) ) {
-                grunt.file.delete( tmp );
-            }
-
-            grunt.file.mkdir( app );
+        erenow: function( fn ) {
+            var self = this,
+            	dist = app + "/" + this.options.dist;
 
             exec( "cp -R * " + app, function() {
                 exec( "cp -R .git " + app, function() {
@@ -104,17 +124,13 @@ module.exports = function( grunt ) {
                 });
             });
 
-            suite = grunt.file.read( __dirname + "/../suite/suite.html" );
-            suite = suite.replace( "{{js}}", this.benchmarks.replaceWith.js );
-            suite = suite.replace( "{{html}}", this.benchmarks.replaceWith.html );
+            return this;
+        },
 
-            grunt.file.copy( this.options.dist, tmp + "/dist.js" );
-
-            grunt.file.recurse( __dirname + "/../suite/", function( abspath, rootdir, subdir, filename ) {
-                grunt.file.copy( abspath, ".perf/" + filename );
+        spawn: function() {
+            phantomjs.spawn( "http://localhost:7000", {
+                options: {}
             });
-
-            grunt.file.write( ".perf/index.html", suite );
 
             return this;
         },
@@ -133,21 +149,47 @@ module.exports = function( grunt ) {
         put: function( result ) {
             result = result[ 0 ];
 
-            this.benchmarks.replaceWith.results.push({
-                name: result.name,
-                hz: Perf.formatNumber( result.hz )
-            });
+            this.benchmarks.replaceWith.results.push( result.hz );
+
+            return this;
+        },
+
+        finish: function() {
+            var results, percentage;
+
+            for ( var benchmark in this.benchmarks ) {
+                results = this.benchmarks[ benchmark ].results;
+
+                if ( results[ 0 ] > results[ 1 ] ) {
+                    percentage = Math.round( (1 - results[ 1 ] / results[ 0 ]) * 100);
+
+                    if ( percentage >= 5 ) {
+                        grunt.log.oklns( benchmark + " became faster by " + percentage + "%" );
+                    }
+                } else if ( results[ 0 ] < results[ 1 ] ) {
+                    percentage = Math.round(1 - results[ 1 ] / results[ 0 ]) * 100;
+
+                    if ( percentage >= 5 ) {
+                    	grunt.log.errorlns( benchmark + " became slower by " + percentage + "%" );
+                    }
+                }
+            }
+
+            grunt.file.delete( ".perf" );
 
             return this;
         },
 
         checkout: function( fn ) {
             var self = this,
-                buildTasks = this.options.buildTasks.join ? this.options.buildTasks.join( " " ) : this.options.buildTasks.join;
+            	dist = app + "/" + this.options.dist,
+                buildTasks = this.options.buildTasks.join ? this.options.buildTasks.join( " " ) : this.options.buildTasks;
 
-            exec( "git checkout -f HEAD~1", {
+            exec( "git checkout -f " + this.reference, {
                 cwd: path.resolve( ".perf/app" )
             }, function() {
+            	grunt.file.delete( dist );
+
                 exec( "grunt " + buildTasks, {
                     cwd: path.resolve( ".perf/app" )
                 }, function() {
